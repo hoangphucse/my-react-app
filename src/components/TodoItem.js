2e6a31fe9b2c59b04ca780b43beb94dae0c6a17c
hoangphuc <phucherose@gmail.com>
2025-12-27 17:03:46 +0700

    chore: format file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
 function TodoItem({ todo, index, toggleTodo, removeTodo }) {
-  return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
-    >
-      {todo.text}
-      <div>
-        <button onClick={() => toggleTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className='todo' style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+            {todo.text}
+            <div>
+                <button onClick={() => toggleTodo(index)}>Complete</button>
+                <button onClick={() => removeTodo(index)}>x</button>
+            </div>
+        </div>
+    );
 }
 
 export default TodoItem;
