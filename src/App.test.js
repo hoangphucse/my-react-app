ea7531e524ee65341f9ec1185acc1afe7a203bac
hoangphuc <phucherose@gmail.com>
2025-12-27 17:05:52 +0700

    chore: update stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,3 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import App from './App';
-
-test('full todo app flow 1', () => {
-    //   // const { getByPlaceholderText, getByText, getAllByText } = render(<App />);
-    //   // const input = getByPlaceholderText('Add a new todo');
-    //   // const form = input.closest('form');
-
-    //   // // add a new todo
-    //   // fireEvent.change(input, { target: { value: 'New Todo' } });
-    //   // fireEvent.submit(form);
-    //   // let todos = getAllByText(/Todo/);
-    //   // expect(todos.length).toBe(1);
-
-    //   // // complete a todo
-    //   // const completeButton = getByText('Complete');
-    //   // fireEvent.click(completeButton);
-    //   // const todo = getByText('New Todo');
-    //   // expect(todo).toHaveStyle('text-decoration: line-through');
-
-    //   // // remove a todo
-    //   // const removeButton = getByText('x');
-    //   // fireEvent.click(removeButton);
-    //   // todos = getAllByText(/Todo/);
-    //   // expect(todos.length).toBe(0);
+test('dummy test', () => {
     expect(true).toBe(true);
 });
